4dec4b272b9dd95a99050a990e00202d02f569f0
vlaselindrive <vladislav.seliutin@indriver.com>
2025-07-21 15:48:23 +0300

    Изменил инкрементальную стратегию 21.07

---
+++ models/revenue_daily.sql
@@ -0,0 +1,14 @@
+select
+    "date",
+    sum(price_rub) as revenue_rub,
+    now() at time zone 'utc' as updated_at
+from {{ ref("trips_prep") }}
+{% if is_incremental() %}
+    where "date" >= (select max("date") - interval '2' day from {{ this }})
+{% else %}
+    where 1=1
+{% endif %}
+group by 
+    "date", 
+    now() at time zone 'utc'
+order by "date"
