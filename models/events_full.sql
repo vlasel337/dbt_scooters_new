507cd4a9092683480ac0edaf069f005636e0ed5a
vlaselindrive <vladislav.seliutin@indriver.com>
2025-07-22 15:43:18 +0300

    Добавил сид с типами ивентов 22.07

---
+++ models/events_full.sql
@@ -0,0 +1,4 @@
+select *
+from {{ ref('events_clean') }}
+left join {{ ref('event_types') }}
+    using (type_id)
---
+++ models/events_stat.sql
@@ -0,0 +1,7 @@
+select
+    count("type" = 'cancel_search' or null)
+      / 
+    cast(count("type" = 'start_search' or null) as float)
+      * 100 as cancel_pct
+from
+    {{ ref("events_full") }}
