f503b788c7e777683a669b8bdaa012c031eb7720
vlaselindrive <vladislav.seliutin@indriver.com>
2025-07-23 12:51:17 +0300

    Добавил макрос updated_at() 23.07

--- models/revenue_daily.sql
+++ models/revenue_daily.sql
@@ -1,7 +1,7 @@
 select
     "date",
     sum(price_rub) as revenue_rub,
-    now() at time zone 'utc' as updated_at
+    {{ updated_at() }}
 from {{ ref("trips_prep") }}
 {% if is_incremental() %}
     where "date" >= (select max("date") - interval '2' day from {{ this }})
--- models/trips_users.sql
+++ models/trips_users.sql
@@ -1,7 +1,8 @@
 select
     t1.*,
     t2.sex,
-    extract(year from t1.started_at) - extract(year from t2.birth_date) as age
+    extract(year from t1.started_at) - extract(year from t2.birth_date) as age,
+    {{ updated_at() }}
 from {{ ref("trips_prep") }} t1
 left join {{ source("scooters_raw","users")}} t2
     on t1.user_id = t2.id
